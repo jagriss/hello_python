8b8ea84949aaedbd02a6a4105716da2a0e647eb8
Jace Grissom <jacegrissom43@gmail.com>
2022-08-08 12:19:22 -0400

    added multiple pages to hello

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ import random
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+@app.route('/')
 @app.route('/hello')
 def hello():
    message = """
@@ -14,6 +15,17 @@ def hello():
    """
    num = random.randint(1, 25)   # Select a random integer from 1 - 25.
    return message.format(num)
+
+@app.route('/goodbye')
+def goodbye():
+   message = "<h2>This is the second page!</h2>"
+   return message
+
+@app.route('/third_page')
+def third_page():
+    message = "<h2>A third page too!</h2>"
+    return message
+
 
 if __name__ == '__main__':
    app.run()
